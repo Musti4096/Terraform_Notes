94357006ea4b60687e4fed404003f83f0324db3e
Mustafa AK <mustafaak4096@gmail.com>
2021-06-24 15:02:38 +0200

    FirstAzureVM

---
+++ FirstAzureVM/main.tf
@@ -0,0 +1,94 @@
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+
+resource "azurerm_resource_group" "main" {
+  name     = var.prefix
+  location = var.location
+}
+
+
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "main" {
+  count               = length(var.name_count)
+  name                = "${var.prefix}-nic-${count.index+1}"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_virtual_machine" "main" {
+  count                 = length(var.name_count)
+  name                  = "vm-${count.index + 1}"
+  location              = azurerm_resource_group.main.location
+  resource_group_name   = azurerm_resource_group.main.name
+  network_interface_ids = ["${element(azurerm_network_interface.main.*.id, count.index)}"]
+  vm_size               = "Standard_DS1_v2"
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  # delete_os_disk_on_termination = true
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  # delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "testadmin"
+    admin_password = "Password1234!"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+    environment = "staging"
+  }
+}
+
+output "virtual_machine_name" {value = "${azurerm_virtual_machine.main.*.name}"}
+
+output "virtual_machine_location" { value = azurerm_virtual_machine.main.*.location}
+
+output "azurerm_subnet_name" { value = azurerm_subnet.internal.name }
---
+++ FirstAzureVM/variable.tf
@@ -0,0 +1,6 @@
+
+variable "prefix" {default = "tfvmex"}
+
+variable "location" { default = "westeurope"}
+
+variable "name_count" { default = ["server1", "server2", "server3"] }
