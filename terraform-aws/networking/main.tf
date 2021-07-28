90e38e3d4fc48c04a21577f8296860202ca62b5b
Musti4096 <mustafaak4096@gmail.com>
2021-07-28 14:00:16 +0200

    random_shuffle functions added

--- terraform-aws/networking/main.tf
+++ terraform-aws/networking/main.tf
@@ -1,10 +1,16 @@
 ####### Networking main.tf ##########
+data "aws_availability_zones" "available" {}
+
 
 resource "random_integer" "random" {
   min = 1
   max = 100
 }
 
+resource "random_shuffle" "az_list" {
+  input        = data.aws_availability_zones.available.names
+  result_count = var.max_subnets
+}
 resource "aws_vpc" "mustafa_vpc" {
   cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
@@ -20,7 +26,7 @@ resource "aws_subnet" "mustafa_public_subnet" {
   vpc_id                  = aws_vpc.mustafa_vpc.id
   cidr_block              = var.public_cidrs[count.index]
   map_public_ip_on_launch = true
-  availability_zone       = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d"][count.index]
+  availability_zone       = random_shuffle.az_list.result[count.index]
 
   tags = {
     Name = "mustafa_public_subnet_${count.index + 1}"
@@ -32,7 +38,7 @@ resource "aws_subnet" "mustafa_private_subnet" {
   vpc_id                  = aws_vpc.mustafa_vpc.id
   cidr_block              = var.private_cidrs[count.index]
   map_public_ip_on_launch = false
-  availability_zone       = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d"][count.index]
+  availability_zone       = random_shuffle.az_list.result[count.index]
 
   tags = {
     Name = "mustafa_private_subnet_${count.index + 1}"
