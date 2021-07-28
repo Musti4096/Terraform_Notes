b6b304ca5ae922fb2e9a3c405f08b8f6757c58f4
Musti4096 <mustafaak4096@gmail.com>
2021-07-28 11:59:49 +0200

    aws vpc created

--- terraform-aws/networking/main.tf
+++ terraform-aws/networking/main.tf
@@ -1,10 +1,16 @@
 ####### Networking main.tf ##########
 
-resource "aws_vpc" "main" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
+resource "random_integer" "random" {
+  min = 1
+  max = 100
+}
+
+resource "aws_vpc" "mustafa_vpc" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support = true
 
   tags = {
-    Name = "main"
+    Name = "mustafa-vpc-${random_integer.random.id}"
   }
 }
