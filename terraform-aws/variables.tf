9dad62f8d917afcfb3b0deaf68e8c7feb8e4fb22
Mustafa AK <mustafaak4096@gmail.com>
2021-08-03 16:56:45 +0200

    updated

--- terraform-aws/variables.tf
+++ terraform-aws/variables.tf
@@ -1,13 +1,26 @@
 # --- root/variables.tf --- #
-
 variable "aws_region" {
   default = "us-east-1"
 }
 variable "access_ip" {
   type = string
 }
-variable "dbname" {}
 
-variable "dbusername" {}
+#---- database variables ----#
 
-variable "dbpassword" {}
+variable "dbname" {
+  type = string
+  description = "DB name"
+}
+
+variable "dbusername" {
+  type = string
+  description = "DB username"
+  sensitive = true
+}
+
+variable "dbpassword" {
+  type = string
+  description = "database password"
+  sensitive = true
+}
