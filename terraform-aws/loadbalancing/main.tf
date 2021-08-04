8be3005132fd65a8fd130f59684eb2d8cdbdf3ed
Mustafa AK <mustafaak4096@gmail.com>
2021-08-04 11:44:11 +0200

    alb_listener added

--- terraform-aws/loadbalancing/main.tf
+++ terraform-aws/loadbalancing/main.tf
@@ -5,3 +5,29 @@ resource "aws_lb" "mustafa_lb" {
   security_groups = [var.public_sg]
   idle_timeout    = 400
 }
+
+resource "aws_lb_target_group" "mustafa_tg" {
+  name     = "mustafa-lb-tg-${substr(uuid(), 0, 3)}"
+  port     = var.tg_port     #80
+  protocol = var.tg_protocol #HTTP
+  vpc_id   = var.vpc_id
+  lifecycle {
+    ignore_changes = [name]
+  }
+  health_check {
+    healthy_threshold   = var.lb_healthy_threshold   #2
+    unhealthy_threshold = var.lb_unhealthy_threshold #2
+    timeout             = var.lb_timeout             #3
+    interval            = var.lb_interval            #30
+  }
+}
+
+resource "aws_lb_listener" "mustafa_lb_listener" {
+  load_balancer_arn = aws_lb.mustafa_lb.arn
+  port              = var.listener_port #80
+  protocol          = var.listener_protocol
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.mustafa_tg.arn
+  }
+}
